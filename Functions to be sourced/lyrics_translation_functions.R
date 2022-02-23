435f3b7a37e439e8acbbb75af5153fd1b71fd623
Nico Schwarzer <58359349+NicoSchwarzer@users.noreply.github.com>
2022-02-23 09:43:57 +0100

    Create lyrics_translation_functions.R

---
+++ Functions to be sourced/lyrics_translation_functions.R
@@ -0,0 +1,54 @@
+
+## function to detect language based on lyrics
+detect_language <- function(lyrics, vocab, num_lang_identifier, model){
+  
+  # little bit of cleaning
+  lyrics <- stringr::str_replace_all(lyrics, "\n", " ")
+  
+  # tokenize the lyrics and unlist
+  lyrics_tok <- unlist(tokenizers::tokenize_words(lyrics))
+  
+  # initialize empty vector to count
+  count_vector <- numeric(length = length(vocab))
+  
+  for (word in 1:length(vocab)){
+    # count the word and store
+    count_vector[word] <- sum(lyrics_tok == vocab[word])
+  }
+  
+  # get relative appearance
+  count_vector <- count_vector/length(lyrics_tok)
+  
+  # get data as input data matrix
+  X <- t(data.matrix(count_vector))
+  
+  # predict the language
+  pred <- predict(model, X)
+  pred <- which.max(pred)
+  
+  # match language
+  pred_lang <- as.character(num_lang_identifier$lang[num_lang_identifier$Num == pred])
+  
+  # return language
+  return(pred_lang)
+}
+
+
+## make numeric/language identifier
+num_lang_identifier <- data.frame(Num = c(1,2,3,4,5),
+                                  lang = c("english", "french", "german", "italian", "spanish"))
+
+
+
+
+
+deepl_key <- "9e12b50a-ca52-96c3-2bcb-d74589cf6818:fx"
+
+
+
+
+
+
+
+
+
